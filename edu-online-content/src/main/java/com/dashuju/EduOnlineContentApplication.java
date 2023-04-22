020c8f04ffb2d3121c3883a4e77a2f2a5516053d
zjp7466 <12791143+zjp7466@user.noreply.gitee.com>
2023-04-22 16:32:31 +0800

    commit1

---
+++ edu-online-content/src/main/java/com/dashuju/EduOnlineContentApplication.java
@@ -0,0 +1,13 @@
+package com.dashuju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduOnlineContentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduOnlineContentApplication.class, args);
+    }
+
+}
